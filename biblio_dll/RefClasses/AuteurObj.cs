fd78798bb2589fc4852c909c53ca478bd7f5ba13
misteratrox <misteratrox@gmail.com>
2014-11-11 11:16:00 +0100

    Ajout de deux nouvelles classes. Le but est de te montrer qu'à partir de ta classe livre on peut dégager encore une nouvelle entité qui aura sa propre classe, l'auteur. De plus j'ai ajouté un peu de code pour te montrer comment gérer le cas des livres écrit par plusieurs auteurs. En effet on stocke directement une liste d'auteurs dans la classe livre. Le plus souvent cette liste n'aura qu'une entrée, mais au moins tu gères tous les cas de figure.

---
+++ biblio_dll/RefClasses/AuteurObj.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace biblio_dll
+{
+	/// <summary>
+	/// Représente l'auteur d'un livre
+	/// </summary>
+	public class AuteurObj
+	{
+		/// <summary>
+		/// Nom de l'auteur
+		/// </summary>
+		private string _nom;
+		/// <summary>
+		/// Prénom de l'auteur
+		/// </summary>
+		private string _prenom;
+
+		/// <summary>
+		/// Properties Nom de l'auteur
+		/// </summary>
+		/// <value>Nom de l'auteur.</value>
+		public string Nom 
+		{
+			get { return _nom; }
+			set { _nom = value;}
+		}
+
+		/// <summary>
+		/// Properties Prenom de l'auteur
+		/// </summary>
+		/// <value>Prenom de l'auteur.</value>
+		public string Prenom 
+		{
+			get { return _prenom; }
+			set { _prenom = value;}
+		}
+
+		/// <summary>
+		/// Constructeur
+		/// </summary>
+		public AuteurObj ()
+		{
+		}
+	}
+}
+
---
+++ biblio_dll/RefClasses/LivreObj.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace biblio_dll
+{
+	/// <summary>
+	/// Représente un livre
+	/// </summary>
+	public class LivreObj
+	{
+		/// <summary>
+		/// Titre du livre
+		/// </summary>
+		private string _titre;
+		/// <summary>
+		/// Auteurs du livre
+		/// </summary>
+		private List<AuteurObj> _auteurs;
+
+		/// <summary>
+		/// Properties du titre
+		/// </summary>
+		/// <value>Le titre du livre</value>
+		public string Titre {
+			get { return _titre; }
+			set { _titre = value;}
+		}
+
+		/// <summary>
+		/// Properties de la liste des auteurs du livre
+		/// </summary>
+		/// <value>le(s) auteur(s) du livre.</value>
+		public List<AuteurObj> Auteurs
+		{
+			get 
+			{
+				if (_auteurs == null) 
+				{
+					// la liste n'est pas encore instanciée. On va donc le faire avec de la renvoyer (pour éviter un null exception)
+					_auteurs = new List<AuteurObj> ();
+				}
+
+				return _auteurs;
+			}
+
+			set 
+			{
+				_auteurs = value;
+			}
+		}
+
+		/// <summary>
+		/// Constructeur
+		/// </summary>
+		public LivreObj ()
+		{
+		}
+	}
+}
+
